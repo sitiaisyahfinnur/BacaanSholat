5a614fe7e238df1494b6472a126ad0805ab6caff
siti aisyah finnur <st.aisyah2002@gmail.com>
2022-07-25 19:28:52 +0800

    initial Commit

---
+++ app/src/main/java/tmj/bacaansholat/ModelBacaan.kt
@@ -0,0 +1,9 @@
+package tmj.bacaansholat
+
+class ModelBacaan {
+    var id: String? = null
+    var name: String? = null
+    var arabic: String? = null
+    var latin: String? = null
+    var terjemahan: String? = null
+}
